fec765b29cec1831119c2dbf1a5e549a84577061
Saygun Icyuz <saygunicyuz@gmail.com>
2021-02-02 01:58:31 +0300

    Override to_json method of Recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,15 @@
 # frozen_string_literal: true
 
 class Recipe
-  attr_accessor :title, :href, :ingredients, :thumbnail
+  attr_accessor :id, :title, :href, :ingredients, :thumbnail
+
+  def to_json(options)
+    {
+      id: self.id,
+      title: self.title,
+      href: self.href,
+      ingredients: self.ingredients,
+      thumbnail: self.thumbnail
+    }.to_json
+  end
 end
