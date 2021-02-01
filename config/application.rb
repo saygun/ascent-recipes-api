62b4d33f32e4373a6d02f978b74762ab8f435eef
Saygun Icyuz <saygunicyuz@gmail.com>
2021-02-01 19:05:05 +0300

    Add lib & app/serializers to autoload path

--- config/application.rb
+++ config/application.rb
@@ -30,5 +30,8 @@ module RecipePuppyApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.autoload_paths << config.root.join('lib')
+    config.autoload_paths << config.root.join('app/serializers')
   end
 end
